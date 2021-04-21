8c067d37770ab0a8d477dffbf198c68393ea742a
Yinghui Linda He <heyinghu@usc.edu>
2021-04-21 02:35:42 -0700

    Save temporary

--- venv/frontend/src/App.js
+++ venv/frontend/src/App.js
@@ -1,58 +1,89 @@
-//import logo from './logo.svg';
-//import './App.css';
+////import logo from './logo.svg';
+////import './App.css';
+////
+////function App() {
+////  return (
+////    <div className="App">
+////      <header className="App-header">
+////        <img src={logo} className="App-logo" alt="logo" />
+////        <p>
+////          Edit <code>src/App.js</code> and save to reload.
+////        </p>
+////        <a
+////          className="App-link"
+////          href="https://reactjs.org"
+////          target="_blank"
+////          rel="noopener noreferrer"
+////        >
+////          Learn React
+////        </a>
+////      </header>
+////    </div>
+////  );
+////}
+////
+////export default App;
+//
+////import logo from './logo.svg';
+////import './App.css';
+////import React, { useEffect, useState } from 'react';
+////import axios from 'axios'
+////
+////function App() {
+////  const [getMessage, setGetMessage] = useState({})
+////
+////  useEffect(()=>{
+////    axios.get('http://localhost:5000/flask/hello').then(response => {
+////      console.log("SUCCESS", response)
+////      setGetMessage(response)
+////    }).catch(error => {
+////      console.log(error)
+////    })
+////
+////  }, [])
+////  return (
+////    <div className="App">
+////      <header className="App-header">
+////        <img src={logo} className="App-logo" alt="logo" />
+////        <p>React + Flask Tutorial</p>
+////        <div>{getMessage.status === 200 ?
+////          <h3>{getMessage.data.message}</h3>
+////          :
+////          <h3>LOADING</h3>}</div>
+////      </header>
+////    </div>
+////  );
+////}
+////
+////export default App;
+//
+//import React from 'react';
+//import { Router, Route, Switch, Redirect } from 'react-router-dom';
+////import { connect } from 'react-redux';
+//
+//import { HomePage } from './Home';
+//import { LoginPage } from './Login';
 //
 //function App() {
-//  return (
-//    <div className="App">
-//      <header className="App-header">
-//        <img src={logo} className="App-logo" alt="logo" />
-//        <p>
-//          Edit <code>src/App.js</code> and save to reload.
-//        </p>
-//        <a
-//          className="App-link"
-//          href="https://reactjs.org"
-//          target="_blank"
-//          rel="noopener noreferrer"
-//        >
-//          Learn React
-//        </a>
-//      </header>
-//    </div>
-//  );
+//
+//    render() {
+//        const { alert } = this.props;
+//        return (
+//            <div className="jumbotron">
+//                <div className="container">
+//                    <div className="col-sm-8 col-sm-offset-2">
+//                        <Router>
+//                            <Switch>
+//                                <Route path="/login" component={Login} />
+//                                <Route path="/register" component={Register} />
+//                                <Redirect from="*" to="/" />
+//                            </Switch>
+//                        </Router>
+//                    </div>
+//                </div>
+//            </div>
+//        );
+//    }
 //}
 //
 //export default App;
-
-import logo from './logo.svg';
-import './App.css';
-import React, { useEffect, useState } from 'react';
-import axios from 'axios'
-
-function App() {
-  const [getMessage, setGetMessage] = useState({})
-
-  useEffect(()=>{
-    axios.get('http://localhost:5000/flask/hello').then(response => {
-      console.log("SUCCESS", response)
-      setGetMessage(response)
-    }).catch(error => {
-      console.log(error)
-    })
-
-  }, [])
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>React + Flask Tutorial</p>
-        <div>{getMessage.status === 200 ?
-          <h3>{getMessage.data.message}</h3>
-          :
-          <h3>LOADING</h3>}</div>
-      </header>
-    </div>
-  );
-}
-
-export default App;
--- venv/frontend/src/Login.js
+++ venv/frontend/src/Login.js
@@ -1,24 +1,96 @@
-import React from 'react';
-import './Login.css';
-//import axios from 'axios'
+//import React from 'react';
+//import './Login.css';
+//import Button from '@material-ui/core/Button';
+//
+////import axios from 'axios'
+//
+//class LoginTab extends React.Component {
+//  render() {
+//    const { classes } = this.props;
+//    return(
+//    <div className="login-wrapper">
+//      <h1>Please Log In</h1>
+//      <form>
+//        <label>
+//          <p>Username</p>
+//          <input type="text" />
+//        </label>
+//        <label>
+//          <p>Password</p>
+//          <input type="password" />
+//        </label>
+//        <div>
+//          <Button variant="contained">
+//            Log In
+//          </Button>
+//        </div>
+//        <div>
+//          <Button variant="contained">
+//            Register
+//          </Button>
+//        </div>
+//        <div>
+//
+//        </div>
+//      </form>
+//    </div>
+//    )
+//  }
+//}
 
-export default function Login() {
-  return(
-    <div className="login-wrapper">
-      <h1>Please Log In</h1>
-      <form>
-        <label>
-          <p>Username</p>
-          <input type="text" />
-        </label>
-        <label>
-          <p>Password</p>
-          <input type="password" />
-        </label>
-        <div>
-          <button type="submit">Submit</button>
-        </div>
-      </form>
-    </div>
-  )
+import React from 'react';
+import { withStyles, Grid, TextField, Button, FormControlLabel, Checkbox } from '@material-ui/core';
+import { Face, Fingerprint } from '@material-ui/icons'
+const styles = theme => ({
+    margin: {
+        margin: theme.spacing.unit * 2,
+    },
+    padding: {
+        padding: theme.spacing.unit
+    }
+});
+
+class Login extends React.Component {
+    render() {
+        const { classes } = this.props;
+        return (
+            <div className="login-wrapper">
+                <div className={classes.margin}>
+                    <Grid container spacing={8} alignItems="flex-end">
+                        <Grid item>
+                            <Face />
+                        </Grid>
+                        <Grid item md={true} sm={true} xs={true}>
+                            <TextField id="username" label="Username" type="email" fullWidth autoFocus required />
+                        </Grid>
+                    </Grid>
+                    <Grid container spacing={8} alignItems="flex-end">
+                        <Grid item>
+                            <Fingerprint />
+                        </Grid>
+                        <Grid item md={true} sm={true} xs={true}>
+                            <TextField id="password" label="Password" type="password" fullWidth required />
+                        </Grid>
+                    </Grid>
+                    <Grid container alignItems="center" justify="space-between">
+                        <Grid item>
+                            <FormControlLabel control={
+                                <Checkbox
+                                    color="primary"
+                                />
+                            } label="Remember me" />
+                        </Grid>
+                        <Grid item>
+                            <Button disableFocusRipple disableRipple style={{ textTransform: "none" }} variant="text" color="primary">Forgot password ?</Button>
+                        </Grid>
+                    </Grid>
+                    <Grid container justify="center" style={{ marginTop: '10px' }}>
+                        <Button variant="outlined" color="primary" style={{ textTransform: "none" }}>Login</Button>
+                    </Grid>
+                </div>
+            </div>
+        );
+    }
 }
+
+export default withStyles(styles)(Login);
